332eb0e25692ef23b5b500f91a3a48922fa2751e
saberdhaouadi <dhaouadis@gmail.com>
2021-05-24 14:06:44 +0100

    add multiply function

--- calculator.py
+++ calculator.py
@@ -6,3 +6,6 @@ def add(first_item, second_item):
 
 def subtract(first_item, second_item):
     return first_item - second_item
+
+def multiply(first_item, second_item):
+    return first_item * second_item
